2be7b4ff8dced1c1433b484343687cabfeacdbd6
OueslatiAhmed <ahmedoueslati122@gmail.com>
2017-05-14 23:54:35 +0200

    add theme

---
+++ header.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Quickly a Blogging Category Flat Bootstarp Resposive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Quickly Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php bloginfo('template_directory'); ?>/css/blog.css"
+rel="stylesheet">
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	
+<body>
+<!-- banner-body -->
+	<div class="banner-body">
+		<div class="container">
+<!-- header -->
+			<div class="header">
+				<div class="header-nav">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						  </button>
+						   <a class="navbar-brand" href="index.html"><span>Q</span>uickly</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						 <ul class="nav navbar-nav">
+							<li class="hvr-bounce-to-bottom active"><a href="index.html">Home</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="about.html">About</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="Portfolio.html">Portfolio</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="Pages.html">Pages</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="contact.html">Contact Us</a></li>
+						  </ul>
+						  <div class="sign-in">
+							<ul>
+								<li><a href="login.html">Sign In </a>/</li>
+								<li><a href="register.html">Register</a></li>
+							</ul>
+							</div>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+				</div>
+	
+			<!-- search-scripts -->
+			<script src="js/classie.js"></script>
+			<script src="js/uisearch.js"></script>
+				<script>
+					new UISearch( document.getElementById( 'sb-search' ) );
+				</script>
+			<!-- //search-scripts -->
+			</div>
+<!-- //header -->
+<!-- header-bottom -->
+<!-- banner-bottom -->
+			<div class="banner-bottom">
+				<ul id="flexiselDemo1">			
+					<li>
+						<div class="banner-bottom-grid">
+							<img src="images/1.jpg" alt=" " class="img-responsive" />
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus 
+								qui blanditiis praesentium voluptatum deleniti atque corrupti 
+								quos dolores et quas molestias excepturi sint occaecati
+								cupiditate non provident</p>
+							<div class="more">
+								<a href="single.html" class="hvr-bounce-to-bottom sint">Read More</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="banner-bottom-grid">
+							<img src="images/2.jpg" alt=" " class="img-responsive" />
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus 
+								qui blanditiis praesentium voluptatum deleniti atque corrupti 
+								quos dolores et quas molestias excepturi sint occaecati
+								cupiditate non provident</p>
+							<div class="more">
+								<a href="single.html" class="hvr-bounce-to-bottom sint">Read More</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="banner-bottom-grid">
+							<img src="images/3.jpg" alt=" " class="img-responsive" />
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus 
+								qui blanditiis praesentium voluptatum deleniti atque corrupti 
+								quos dolores et quas molestias excepturi sint occaecati
+								cupiditate non provident</p>
+							<div class="more">
+								<a href="single.html" class="hvr-bounce-to-bottom sint">Read More</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<script type="text/javascript">
+							$(window).load(function() {
+								$("#flexiselDemo1").flexisel({
+									visibleItems: 3,
+									animationSpeed: 1000,
+									autoPlay: false,
+									autoPlaySpeed: 3000,    		
+									pauseOnHover: true,
+									enableResponsiveBreakpoints: true,
+									responsiveBreakpoints: { 
+										portrait: { 
+											changePoint:480,
+											visibleItems: 1
+										}, 
+										landscape: { 
+											changePoint:640,
+											visibleItems:2
+										},
+										tablet: { 
+											changePoint:768,
+											visibleItems: 3
+										}
+									}
+								});
+								
+							});
+					</script>
+					<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+			</div>
+<!-- //banner-bottom -->
---
+++ index.php
@@ -0,0 +1,26 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+	<?php get_header(); ?>
+<!-- blog -->
+	<?php get_sidebar();?>		
+<!-- //blog -->
+	</div>
+<!-- //header-bottom -->
+		</div>
+	</div>
+<!-- //banner-body -->
+<!-- footer -->
+	<?php get_footer(); ?>
+<!-- //footer -->
+<!-- for bootstrap working -->
+		<script src="js/bootstrap.js"> </script>
+<!-- //for bootstrap working -->
+</body>
+</html>
+<!--
+just a comment added here by chehir
+-->

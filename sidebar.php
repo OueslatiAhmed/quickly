03d27d3906e1316f207888ef4a4cfbaba9110cb3
OueslatiAhmed <ahmedoueslati122@gmail.com>
2017-05-17 17:07:22 +0200

    add functions

---
+++ sidebar.php
@@ -0,0 +1,21 @@
+<!-- search-scripts -->
+			<script src="js/classie.js"></script>
+			<script src="js/uisearch.js"></script>
+				<script>
+					new UISearch( document.getElementById( 'sb-search' ) );
+				</script>
+			<!-- //search-scripts -->
+			</div>
+<!-- //header -->
+<!-- header-bottom -->
+	<div class="header-bottom">
+		<div class="header-bottom-top">
+			<ul>
+				<li><a href="#" class="g"> </a></li>
+				<li><a href="#" class="p"> </a></li>
+				<li><a href="#" class="facebook"> </a></li>
+				<li><a href="#" class="twitter"> </a></li>
+			</ul>
+		</div>
+		<div class="clearfix"> </div>
+		</div>
